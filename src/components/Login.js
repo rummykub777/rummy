abf461c49ba30a4ba9d6599dec5be1b3dcd69002
Parminder Kaur <parminder@fkointech.com>
2023-03-18 11:16:38 +0530

    login modal

---
+++ src/components/Login.js
@@ -0,0 +1,42 @@
+import Modal from "react-modal/lib/components/Modal"
+import React from "react"
+
+export default function Login(props) {
+    const { isOpen, onClose } = props
+
+    return (
+        <Modal isOpen={isOpen} onRequestClose={onClose} contentLabel="My dialog" className="mymodal auth-modal" overlayClassName="myoverlay auth-overlay" closeTimeoutMS={500} ariaHideApp={false}>
+            <div className="" style={{ width: "100%" }}>
+                <div className="custom-modal-header">
+                    <p>Login</p>
+                    <div className="close-btn" onClick={onClose}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="close-btn" width="16" height="16" viewBox="0 0 24 24">
+                            <path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z" />
+                        </svg>
+                    </div>
+                </div>
+                <div className="">
+                    <div className="form-group">
+                        <label className="input__label">
+                            Phone
+                            <span className="required ms-1 st-fs-12"> *</span>
+                        </label>
+                        <input
+                            type="number"
+                            className="input__field"
+                            placeholder="Phone"
+                            onChange={(e) => { }} />
+                    </div>
+                </div>
+                <div className=" btn-group">
+                    <button className="btn-cancel" type="button">
+                        Cancel
+                    </button>
+                    <button className="btn-primary" type="button">
+                        Login
+                    </button>
+                </div>
+            </div>
+        </Modal>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,46 +1,53 @@
-import React from "react"
+import React, { useState } from "react"
+import Login from "../components/Login"
 export default function Home() {
-    return (<><section className="rummy desktop_view">
-        <div className="header">
-            <div className="header_container">
-                <div className="user_profile">
-                    <div className="user_container">
-                        <img src="/assets/users.png" height={150} width={150} alt="user" />
-                        <img src="/assets/star.png" height={20} width={20} alt="user" />
-                    </div>
-                    <div className="user_info">
-                        <div className="user_heading">User Name</div>
-                        <div className="user_name">Eli Mark</div>
-                    </div>
-                </div>
-                <div className="nav_bar">
-                    <img src="/assets/nav.png" className="nav_bg_img" alt="rummy" />
-                    <div className="nav_container">
-                        <div className="">
-                            <img src="/assets/trophy.png" alt="rummy" />
-                            <p>100</p>
+    const [openModal, setOpenModal] = useState(false)
+    return (<>
+        {openModal && <Login
+            isOpen={openModal}
+            onClose={() => setOpenModal(!openModal)}
+        />}
+        <section className="rummy desktop_view">
+            <div className="header">
+                <div className="header_container">
+                    <div className="user_profile" onClick={() => { setOpenModal(true) }}>
+                        <div className="user_container">
+                            <img src="/assets/users.png" height={150} width={150} alt="user" />
+                            <img src="/assets/star.png" height={20} width={20} alt="user" />
                         </div>
-                        <div className="">
-                            <img src="/assets/coin.png" alt="rummy" />
-                            <p>100</p>
+                        <div className="user_info">
+                            <div className="user_heading">User Name</div>
+                            <div className="user_name">Eli Mark</div>
                         </div>
                     </div>
-                </div>
-                <div className="lock_key">
-                    <img src="/assets/lock.png" height={32} width={32} alt="user" />
+                    <div className="nav_bar">
+                        <img src="/assets/nav.png" className="nav_bg_img" alt="rummy" />
+                        <div className="nav_container">
+                            <div className="">
+                                <img src="/assets/trophy.png" alt="rummy" />
+                                <p>100</p>
+                            </div>
+                            <div className="">
+                                <img src="/assets/coin.png" alt="rummy" />
+                                <p>100</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lock_key">
+                        <img src="/assets/lock.png" height={32} width={32} alt="user" />
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="rummy-wrapper">
-            <div className="body_container">
-                <div className="app_logo"><img src="/assets/logo.png" alt="rummy" /></div>
-                <div className="rummy_cards">
-                    <div className="rummy_card"><img src="/assets/card1.png" alt="card" /></div>
-                    <div className="rummy_card"><img src="/assets/card2.png" alt="card" /></div>
+            <div className="rummy-wrapper">
+                <div className="body_container">
+                    <div className="app_logo"><img src="/assets/logo.png" alt="rummy" /></div>
+                    <div className="rummy_cards">
+                        <div className="rummy_card"><img src="/assets/card1.png" alt="card" /></div>
+                        <div className="rummy_card"><img src="/assets/card2.png" alt="card" /></div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
         <section className="mobile_view">
             <div className="header">
                 <div className="nav_bar">
@@ -57,7 +64,7 @@ export default function Home() {
                     </div>
                 </div>
                 <div className="header_container">
-                    <div className="user_profile">
+                    <div className="user_profile" onClick={() => { setOpenModal(true) }}>
                         <div className="user_container">
                             <img src="/assets/users.png" height={150} width={150} alt="user" />
                             <img src="/assets/star.png" className="star" height={20} width={20} alt="user" />
