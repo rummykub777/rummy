576f609a6cbc71dfae7126ab821993c4956bf79f
Gurwinder Singh <whogurwinder@gmail.com>
2023-03-21 14:02:09 +0530

    link

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,15 @@
+import { Link } from "react-router-dom"
 
 const Cards = () => {
     return <div className="rummy-wrapper">
         <div className="body_container">
             <div className="app_logo"><img src="/assets/logo.png" alt="rummy" /></div>
             <div className="rummy_cards">
-                <div className="rummy_card"><img src="/assets/card1.png" alt="card" /></div>
-                <div className="rummy_card"><img src="/assets/card2.png" alt="card" /></div>
+                <Link to={"/game"} ><div className="rummy_card"><img src="/assets/card1.png" alt="card" /></div></Link>
+                <Link to={"/game"} ><div className="rummy_card"><img src="/assets/card2.png" alt="card" /></div></Link>
             </div>
         </div>
-    </div>
+    </div >
 }
 
 export default Cards
